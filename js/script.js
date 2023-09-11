7412c3b8bd27c25f2d5d188616ad7dc12250af3d
Fakhreza Akbar Susilo <fakhreza.susilo88@gmail.com>
2023-09-11 20:04:27 +0700

    add logic

--- js/script.js
+++ js/script.js
@@ -14,3 +14,20 @@ function openSquare(evt, squareMethod) {
     document.getElementById(squareMethod).style.display = "block";
     evt.currentTarget.className += " active";
 }
+function luasPersegi() {
+    const panjangSisi = parseFloat(document.getElementById("panjang sisi").value);
+    
+
+    const luas = panjangSisi * panjangSisi;
+    document.getElementById("rumusLuas").textContent = panjangSisi+" x "+ panjangSisi ;
+    document.getElementById("result").textContent = luas;
+}
+
+function kelilingPersegi(){
+    const panjangSisi = parseFloat(document.getElementById("panjang sisi2").value);
+    
+
+    const keliling = panjangSisi * 4;
+    document.getElementById("rumusKeliling").textContent = panjangSisi;
+    document.getElementById("result2").textContent = keliling;
+}
